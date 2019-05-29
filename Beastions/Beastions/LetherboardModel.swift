90f1d8c0defc0d3a55aac7aa55a35cd992ea98fd
Riccardo Feletig <riccardo.feletig@tecnicosuperiorekennedy.it>
2019-05-29 14:13:25 +0200

    Commit

---
+++ Beastions/Beastions/LetherboardModel.swift
@@ -0,0 +1,16 @@
+//
+//  LetherboardModel.swift
+//  Beastions
+//
+//  Created by Riccardo Feletig on 22/05/2019.
+//  Copyright © 2019 MO2S. All rights reserved.
+//
+
+import Foundation
+
+struct LetherboardModel {
+    var Id : Int
+    var Name : String
+    var Record : Int
+    var Character : String
+}
